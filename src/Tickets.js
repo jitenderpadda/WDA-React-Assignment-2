c1b7ed12b19c41f96d40297ba9f2e3079b52a4ec
Jitender S. Padda <jitenderpadda@gmail.com>
2017-10-03 14:46:03 +1100

    Update

--- src/Tickets.js
+++ src/Tickets.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import MobileTearSheet from './MobileTearSheet';
 import Editor from "./Editor";
 import {List, ListItem} from 'material-ui/List';
 import Divider from 'material-ui/Divider';
@@ -31,76 +30,74 @@ const rightIconMenu = (
 
 const Tickets = () => (
     <div>
-        {/*<MobileTearSheet>*/}
-            <List  style={{backgroundColor: darkBlack}}>
-                <Subheader>Tickets</Subheader>
-                <ListItem
-                    leftAvatar={<Avatar src="images/ok-128.jpg" />}
-                    rightIconButton={rightIconMenu}
-                    primaryText="Brendan Lim"
-                    secondaryText={
-                        <p>
-                            <span style={{color: darkBlack}}>Brunch this weekend?</span><br />
-                            I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
-                        </p>
-                    }
-                    secondaryTextLines={2}
-                />
-                <Divider inset={true} />
-                <ListItem
-                    leftAvatar={<Avatar src="images/kolage-128.jpg" />}
-                    rightIconButton={rightIconMenu}
-                    primaryText="me, Scott, Jennifer"
-                    secondaryText={
-                        <p>
-                            <span style={{color: darkBlack}}>Summer BBQ</span><br />
-                            Wish I could come, but I&apos;m out of town this weekend.
-                        </p>
-                    }
-                    secondaryTextLines={2}
-                />
-                <Divider inset={true} />
-                <ListItem
-                    leftAvatar={<Avatar src="images/uxceo-128.jpg" />}
-                    rightIconButton={rightIconMenu}
-                    primaryText="Grace Ng"
-                    secondaryText={
-                        <p>
-                            <span style={{color: darkBlack}}>Oui oui</span><br />
-                            Do you have any Paris recs? Have you ever been?
-                        </p>
-                    }
-                    secondaryTextLines={2}
-                />
-                <Divider inset={true} />
-                <ListItem
-                    leftAvatar={<Avatar src="images/kerem-128.jpg" />}
-                    rightIconButton={rightIconMenu}
-                    primaryText="Kerem Suer"
-                    secondaryText={
-                        <p>
-                            <span style={{color: darkBlack}}>Birthday gift</span><br />
-                            Do you have any ideas what we can get Heidi for her birthday? How about a pony?
-                        </p>
-                    }
-                    secondaryTextLines={2}
-                />
-                <Divider inset={true} />
-                <ListItem
-                    leftAvatar={<Avatar src="images/raquelromanp-128.jpg" />}
-                    rightIconButton={rightIconMenu}
-                    primaryText="Raquel Parrado"
-                    secondaryText={
-                        <p>
-                            <span style={{color: darkBlack}}>Recipe to try</span><br />
-                            We should eat this: grated squash. Corn and tomatillo tacos.
-                        </p>
-                    }
-                    secondaryTextLines={2}
-                />
+        <List  style={{backgroundColor: darkBlack}}>
+            <Subheader>Tickets</Subheader>
+            <ListItem
+                leftAvatar={<Avatar src="images/ok-128.jpg" />}
+                rightIconButton={rightIconMenu}
+                primaryText="Brendan Lim"
+                secondaryText={
+                    <p>
+                        <span style={{color: darkBlack}}>Brunch this weekend?</span><br />
+                        I&apos;ll be in your neighborhood doing errands this weekend. Do you want to grab brunch?
+                    </p>
+                }
+                secondaryTextLines={2}
+            />
+            <Divider inset={true} />
+            <ListItem
+                leftAvatar={<Avatar src="images/kolage-128.jpg" />}
+                rightIconButton={rightIconMenu}
+                primaryText="me, Scott, Jennifer"
+                secondaryText={
+                    <p>
+                        <span style={{color: darkBlack}}>Summer BBQ</span><br />
+                        Wish I could come, but I&apos;m out of town this weekend.
+                    </p>
+                }
+                secondaryTextLines={2}
+            />
+            <Divider inset={true} />
+            <ListItem
+                leftAvatar={<Avatar src="images/uxceo-128.jpg" />}
+                rightIconButton={rightIconMenu}
+                primaryText="Grace Ng"
+                secondaryText={
+                    <p>
+                        <span style={{color: darkBlack}}>Oui oui</span><br />
+                        Do you have any Paris recs? Have you ever been?
+                    </p>
+                }
+                secondaryTextLines={2}
+            />
+            <Divider inset={true} />
+            <ListItem
+                leftAvatar={<Avatar src="images/kerem-128.jpg" />}
+                rightIconButton={rightIconMenu}
+                primaryText="Kerem Suer"
+                secondaryText={
+                    <p>
+                        <span style={{color: darkBlack}}>Birthday gift</span><br />
+                        Do you have any ideas what we can get Heidi for her birthday? How about a pony?
+                    </p>
+                }
+                secondaryTextLines={2}
+            />
+            <Divider inset={true} />
+            <ListItem
+                leftAvatar={<Avatar src="images/raquelromanp-128.jpg" />}
+                rightIconButton={rightIconMenu}
+                primaryText="Raquel Parrado"
+                secondaryText={
+                    <p>
+                        <span style={{color: darkBlack}}>Recipe to try</span><br />
+                        We should eat this: grated squash. Corn and tomatillo tacos.
+                    </p>
+                }
+                secondaryTextLines={2}
+            />
 
-            </List>
-        {/*</MobileTearSheet>*/}
+        </List>
 
         {/*Editor*/}
         <Editor/>
